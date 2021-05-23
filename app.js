6858279963b4f449e9f3e8a20a542310d2cfeaed
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-05-23 16:43:13 +0300

    add some java

---
+++ app.js
@@ -0,0 +1,14 @@
+let vipAsk = prompt('hello, did you have account')
+
+if (vipAsk == 'yes'){
+    userName = prompt('Please enter your username ')
+  } else {
+    userName = prompt('Please enter your name ')
+  }
+
+  alert ("Nice to meet you "+ userName )  
+
+  let askMovie1 = prompt('what type of movie did you like ?')
+  let askMovie2 = prompt('please enter name of you fav movie')
+
+  alert( userName + 'you like ' + askMovie1 + 'movies and your fav movie is ' + askMovie2 )
